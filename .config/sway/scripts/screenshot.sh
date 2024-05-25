5fc4f55fe0b353f82a88210c0d9efc7fc827928a
ablizno <ablizno@gmail.com>
2024-05-25 18:12:42 -0400

    fixed screenshots

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -2,17 +2,17 @@
  
 entries="Active Screen Output Area Window"
  
-selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
+selected=$(printf '%s\n' $entries | fuzzel -d -w 15 -l 5 -p "Screenshot what?" | awk '{print tolower($1)}')
  
 case $selected in
   active)
-    /usr/share/sway-contrib/scripts/grimshot --notify save active;;
+    /usr/share/sway-contrib/grimshot --notify save active;;
   screen)
-    /usr/share/sway-contrib/scripts/grimshot --notify save screen;;
+    /usr/share/sway-contrib/grimshot --notify save screen;;
   output)
-    /usr/share/sway-contrib/scripts/grimshot --notify save output;;
+    /usr/share/sway-contrib/grimshot --notify save output;;
   area)
-    /usr/share/sway-contrib/scripts/grimshot --notify save area;;
+    /usr/share/sway-contrib/grimshot --notify save area;;
   window)
-    /usr/share/sway-contrib/scripts/grimshot --notify save window;;
+    /usr/share/sway-contrib/grimshot --notify save window;;
 esac
